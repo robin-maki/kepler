657a2e188c68ec3be53d52d658adbd45b2396791
robin* <robin_maki@planet.moe>
2023-06-05 01:02:39 +0900

    Navbar에 기여하기 버튼 추가

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,11 @@
       Kepler
       <small style="font-size: 0.7em; color: #555; vertical-align: center;"> 네트워킹 행사 일정 목록</small>
     </a>
+    <ul class="navbar-nav d-flex">
+      <li class="nav-item">
+        <a href="https://github.com/robin-maki/kepler" class="nav-link"><i class="bi bi-github"></i> 기여하기</a>
+      </li>
+    </ul>
   </div>
 </nav>
 <div class="container">
